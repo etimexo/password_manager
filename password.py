209e49fdff5f66a412a861574870bea424dbebed
Elijah Obisesan <elijahobisesan01@gmail.com>
2024-03-20 22:46:08 +0100

    ...

--- password.py
+++ password.py
@@ -1,4 +1,3 @@
-master_pwd = input("What is the master password? ")
 mode = input("Do you want to add a new password or view existing ones? (add/view) ")
 def add():
     name = input('Input account name: ')
@@ -7,10 +6,12 @@ def add():
         f.write(name + pwd + "\n")
 def view():
     with open('passwords.txt', 'r') as f:
+        
         for line in f.readlines():
             data = line.rstrip()
-            user, password =  data.split(": ")
-            print("User: ", user, "\n Password: ", password)
+            print()
+            user, passw =  data.split(": ")
+            print(user, "\n Password: ",passw)
 
 if mode == "add":
     add()
